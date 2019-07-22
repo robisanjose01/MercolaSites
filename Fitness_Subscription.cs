26ef712b10d5f70e332b6f0f12b26d77436a9a93
kevinbryllebagnas <kevinbryllebagnas@gmail.com>
2019-07-22 17:54:28 +0800

    Add files via upload

---
+++ Fitness_Subscription.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Subscription_Test.Lib;
+
+namespace Subscription_Test.Pages
+{
+    public class Fitness_Subscription
+    {
+        private OpenBrowser _test;
+        
+        public Fitness_Subscription(OpenBrowser methods)
+        {
+            _test = methods;
+        }
+            List<String> list = new List<String>();
+            public IAlert HeaderAlert1;
+            public String HeaderAlert2;
+            public String HeaderAlert3;
+            public String invalid1 = " ";
+            public String invalid2 = "asdasdawd@#$.com";
+            public String invalid3 = "#$@yahoo.com";
+            public String invalid4 = "asdawdawd@yahoo.gom";
+            public String valid1 = "fhtest10@yahoo.com";
+            public String valid2 = "roberts@mercola.com";
+
+            public By sub_header = By.Id("NLSubscriptionTop_NewsletterSubscriptionTop_emailAddress");
+            public By sub_header_button = By.Id("NLSubscriptionTop_NewsletterSubscriptionTop_Subscribe");
+        
+        public void FitnessHeader_Invalid()
+        {
+            list.Add(invalid1);
+            list.Add(invalid2);
+            list.Add(invalid3);
+            list.Add(invalid4);
+            
+         foreach (var fitinvalid in list)
+         {
+             IWebDriver driver = _test.driver;
+             driver.FindElement(sub_header).SendKeys(fitinvalid);
+             driver.FindElement(sub_header_button).Click();
+             Thread.Sleep(3000);
+             HeaderAlert1 = driver.SwitchTo().Alert();
+             HeaderAlert2 = HeaderAlert1.Text;
+             HeaderAlert1.Accept();
+             Thread.Sleep(500);
+             IWebElement clear = driver.FindElement(sub_header);
+             clear.Clear();
+
+             HeaderAlert3 = HeaderAlert2;
+             Assert.AreEqual(HeaderAlert3, HeaderAlert2);
+             Console.WriteLine("Email Address: " + fitinvalid + "Message: " + HeaderAlert2);
+             
+          }
+          }
+
+         public void FitnessHeaderNewEmail()
+          {
+            IWebDriver driver = _test.driver;
+            driver.FindElement(sub_header).SendKeys(valid1);
+            driver.FindElement(sub_header_button).Click();
+            Thread.Sleep(3000);
+            
+
+          }
+      }
+  }
+
+
---
+++ UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Subscription_Test.Lib;
+using Subscription_Test.Pages;
+
+namespace Subscription_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        OpenBrowser _test;
+        Fitness_Subscription _fs;
+       
+        [TestInitialize]
+        public void setup()
+        {
+            _test = new OpenBrowser();
+            _fs = new Fitness_Subscription(_test);
+            
+        }
+        
+        [TestMethod]
+        public void FitnessSub()
+        {
+            _test.InitializeBrowse();
+            Thread.Sleep(3000);
+            _fs.FitnessHeader_Invalid();
+            _fs.FitnessHeaderNewEmail();
+        }
+    }
+}
+
